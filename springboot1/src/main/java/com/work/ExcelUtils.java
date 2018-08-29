7672d1fbd0651f91518f821561145e4531add237
jackylee <1192964153@qq.com>
2018-08-29 22:04:21 +0800

    todo set cellstyle

--- springboot1/src/main/java/com/work/ExcelUtils.java
+++ springboot1/src/main/java/com/work/ExcelUtils.java
@@ -3,11 +3,8 @@ package com.work;
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fundway.trafficsignal.business.tc.entity.ExcelDemo;
-import com.fundway.trafficsignal.business.tc.entity.ExcelModel;
 import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 
 import javax.servlet.http.HttpServletResponse;
@@ -72,30 +69,32 @@ public class ExcelUtils {
 
         List<ExcelDemo> excelEntityList = model.getDemoList();
 
-        boolean updateFlag = false;
-        boolean noteFlag = false;
         String headersStr = getNotNullFieldNames(excelEntityList);
 
         if (headersStr == null){
             return;
         }
 
-        int headerNum = headersStr.split("|").length;
-        createHeaders(sheet,headersStr);
+        int headerNum = headersStr.split("\\|").length;
+        createHeaders(workbook,sheet,headersStr);
 
         //自动调整列宽,这个必须在最后设置，否则可能无效
         for(int n=0;n<headerNum;n++){
             sheet.autoSizeColumn(n);
         }
-        HSSFCellStyle cellStyle = workbook.createCellStyle();
-        cellStyle.setAlignment(HorizontalAlignment.CENTER);
 
-        File savefile = new File("E:/");
+        //TODO 每一行遍历cell进行样式设置
+        HSSFRow row = sheet.getRow(2);
+        int col_num = row.getPhysicalNumberOfCells();
+        for (int col=0;col<col_num;col++){
+        }
+
+        File savefile = new File("/home/jackylee/files");
         if (!savefile.exists()) {
             savefile.mkdirs();
         }
         try {
-            FileOutputStream fos = new FileOutputStream("E:/test.xls");
+            FileOutputStream fos = new FileOutputStream("/home/jackylee/files/test.xls");
             workbook.write(fos);
             fos.close();
         } catch (IOException e1) {
@@ -104,89 +103,6 @@ public class ExcelUtils {
 //        ExcelExport(workbook,response,"信号路口详情列表.xls");
     }
     
-    /*public static void excelExport(*//*HttpServletResponse response,*//* ExcelModel model){
-        List<ExcelDemo> data = model.getDemoList();
-        TemplateExportParams params = new TemplateExportParams("e:\\01.xls");
-        params.setHeadingStartRow(2);
-        params.setHeadingRows(1);
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("c1",model.isC1());
-        map.put("c2",model.isC2());
-        map.put("c3",model.isC3());
-        map.put("c4",model.isC4());
-        map.put("c5",model.isC5());
-        map.put("c6",model.isC6());
-        map.put("c7",model.isC7());
-        map.put("c8",model.isC8());
-        map.put("c9",model.isC9());
-        map.put("c10",model.isC10());
-        map.put("c11",model.isC11());
-        map.put("c12",model.isC12());
-        map.put("c13",model.isC13());
-        map.put("c14",model.isC14());
-        map.put("c15",model.isC15());
-        List<Map<String, String>> listMap = new ArrayList<Map<String, String>>();
-        for (int i = 0; i < data.size(); i++) {
-            Map<String, String> lm = new HashMap<String, String>();
-            lm.put("id", i + 1 + "");
-            lm.put("crossname", data.get(i).getCrossname());
-            lm.put("crossno", data.get(i).getCrossno());
-            lm.put("crosstype", data.get(i).getCrosstype());
-            lm.put("managerunit",data.get(i).getManagerunit());
-            lm.put("manager",data.get(i).getManager());
-            lm.put("degree",data.get(i).getDegree());
-            lm.put("isenabled",data.get(i).getIsenabled());
-            lm.put("networkenabled",data.get(i).getNetworkenabled());
-            lm.put("webtype",data.get(i).getWebtype());
-            lm.put("electricannno",data.get(i).getElectricannno());
-            lm.put("crossnumber",data.get(i).getCrossnumber());
-            lm.put("updatetime",data.get(i).getUpdatetime());
-            lm.put("note",data.get(i).getNote());
-            lm.put("annfactory",data.get(i).getAnnfactory());
-            lm.put("anntype",data.get(i).getAnntype());
-            lm.put("devicemaintain",data.get(i).getDevicemaintain());
-            lm.put("laneEentrance",data.get(i).getLaneEentrance());
-            lm.put("laneWentrance",data.get(i).getLaneWentrance());
-            lm.put("laneSentrance",data.get(i).getLaneSentrance());
-            lm.put("laneNentrance",data.get(i).getLaneNentrance());
-            lm.put("laneotherentrance",data.get(i).getLaneotherentrance());
-            lm.put("turnrighttype",data.get(i).getTurnrighttype());
-            lm.put("lightEentrance",data.get(i).getLightEentrance());
-            lm.put("lightWentrance",data.get(i).getLightWentrance());
-            lm.put("lightSentrance",data.get(i).getLightSentrance());
-            lm.put("lightNentrance",data.get(i).getLightNentrance());
-            lm.put("lightotherentrance",data.get(i).getLightotherentrance());
-            lm.put("righttrafficlightcontrol",data.get(i).getRighttrafficlightcontrol());
-            lm.put("trunklinecoordination",data.get(i).getTrunklinecoordination());
-            lm.put("releasetype",data.get(i).getReleasetype());
-            lm.put("specialcontrol",data.get(i).getSpecialcontrol());
-            lm.put("selfadaptioncontrol",data.get(i).getSelfadaptioncontrol());
-            lm.put("yellowtwinkle",data.get(i).getYellowtwinkle());
-            lm.put("flownum",data.get(i).getFlownum());
-            lm.put("phasetimenum",data.get(i).getPhasetimenum());
-            lm.put("maxpersonwaitingtime",data.get(i).getMaxpersonwaitingtime());
-            lm.put("maxcarwaitingtime",data.get(i).getMaxcarwaitingtime());
-            lm.put("maxperiod",data.get(i).getMaxperiod());
-            lm.put("minperiod",data.get(i).getMinperiod());
-            lm.put("yellowtwinkle",data.get(i).getYellowtwinkle());
-            listMap.add(lm);
-        }
-        map.put("maplist", listMap);
-
-        Workbook workbook = ExcelExportUtil.exportExcel(params, map);
-        File savefile = new File("E:/");
-        if (!savefile.exists()) {
-            savefile.mkdirs();
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream("E:/test.xls");
-            workbook.write(fos);
-            fos.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }*/
-
     /**
      *  获取数据的非空字段名字符串
      * @param data 数据
@@ -203,12 +119,12 @@ public class ExcelUtils {
         try {
             for(Field f : fields){
                 f.setAccessible(true);
-                Object fieldName;
-                fieldName = f.get(data.get(0));
-                if(fieldName == null){
+                Object obj;
+                obj = f.get(data.get(0));
+                if(obj == null){
                     continue;
                 }
-                sb.append(fieldName+",");
+                sb.append(f.getName()+"|");
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
@@ -220,8 +136,8 @@ public class ExcelUtils {
      * 根据字符串创建表头
      * @param str
      */
-    public static void createHeaders(HSSFSheet sheet,String str){
-        String[] headers = str.split("|");
+    public static void createHeaders(Workbook workbook,HSSFSheet sheet,String str){
+        String[] headers = str.split("\\|");
         String annStr = "annfactory|anntype|devicemaintain";
         //TODO
         String timingStr = "trunklinecoordination|releasetype|specialcontrol|selfadaptioncontrol|yellowtwinkle|flownum|" +
@@ -246,7 +162,7 @@ public class ExcelUtils {
                 basicHeader.append(h+"|");
             }
         }
-        int headerNum = basicHeader.toString().split("|").length;
+        int headerNum = basicHeader.toString().split("\\|").length;
         if (annHeader.toString().length()>0){
             headerNum += 3;
             //id列
@@ -273,11 +189,22 @@ public class ExcelUtils {
 
         //在excel表中添加第一行表头
         HSSFRow row = sheet.createRow(0);
+        row.setHeight((short) 700);
         HSSFCell cell1 = row.createCell(0);
-        sheet.addMergedRegion(new CellRangeAddress(0,0,0,headerNum-1));
+        //一级大标题样式
+        Font font = workbook.createFont();
+        font.setFontName("仿宋_GB2312");
+        font.setBold(true);//粗体显示
+        font.setFontHeightInPoints((short) 20);
+        CellStyle centerStyle = workbook.createCellStyle();
+        centerStyle.setFont(font);
+        centerStyle.setAlignment(HorizontalAlignment.CENTER_SELECTION);
+        centerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        sheet.addMergedRegion(new CellRangeAddress(0,0,0,headerNum));
         cell1.setCellValue("信号路口详情列表");
+        cell1.setCellStyle(centerStyle);
 
-        int temp = headerNum;
+        int temp = basicHeader.toString().split("\\|").length;
         if (updatetimeFlag){
             temp -= 1;
         }
@@ -298,46 +225,56 @@ public class ExcelUtils {
         row1.createCell(colNum++).setCellValue("序号");
         row1.createCell(colNum++).setCellValue("路口名称");
         row1.createCell(colNum++).setCellValue("路口编号");
-        for (String s : headers){
-            if (s.contains("crosstype")){
+        headerStr.append("crossname|crossno|");
+        for (String s : basicHeader.toString().split("\\|")){
+            if (s.matches("crosstype")){
                 row1.createCell(colNum++).setCellValue("路口类型");
                 headerStr.append("crosstype|");
+                continue;
             }
-            if (s.contains("managerunit")){
+            if (s.matches("managerunit")){
                 row1.createCell(colNum++).setCellValue("管理单位");
                 headerStr.append("managerunit|");
+                continue;
             }
-            if (s.contains("manager")){
+            if (s.matches("manager")){
                 row1.createCell(colNum++).setCellValue("辖区中队");
                 headerStr.append("manager|");
+                continue;
             }
-            if (s.contains("degree")){
+            if (s.matches("degree")){
                 row1.createCell(colNum++).setCellValue("重要程度");
                 headerStr.append("degree|");
+                continue;
             }
-            if (s.contains("isenabled")){
+            if (s.matches("isenabled")){
                 row1.createCell(colNum++).setCellValue("启用状态");
                 headerStr.append("isenabled|");
+                continue;
             }
-            if (s.contains("networkenabled")){
+            if (s.matches("networkenabled")){
                 row1.createCell(colNum++).setCellValue("联网状态");
                 headerStr.append("networkenabled|");
+                continue;
             }
-            if (s.contains("webtype")){
+            if (s.matches("webtype")){
                 row1.createCell(colNum++).setCellValue("联网方式");
                 headerStr.append("webtype|");
+                continue;
             }
-            if (s.contains("electricannno")){
+            if (s.matches("electricannno")){
                 row1.createCell(colNum++).setCellValue("电子监控编号");
                 headerStr.append("electricannno|");
+                continue;
             }
-            if (s.contains("crossnumber")){
+            if (s.matches("crossnumber")){
                 row1.createCell(colNum++).setCellValue("卡口编号");
                 headerStr.append("crossnumber|");
+                continue;
             }
         }
         if (annHeader.toString().length()>0) {
-            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum,colNum+2));
+            sheet.addMergedRegion(new CellRangeAddress(1,1,colNum,colNum+2));
             row1.createCell(colNum).setCellValue("信号机设备信息");
             row2.createCell(colNum++).setCellValue("厂家");
             row2.createCell(colNum++).setCellValue("型号");
@@ -345,7 +282,7 @@ public class ExcelUtils {
             headerNum += 3;
         }
         if (laneHeader.toString().length()>0) {
-            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum,colNum+5));
+            sheet.addMergedRegion(new CellRangeAddress(1,1,colNum,colNum+5));
             row1.createCell(colNum).setCellValue("车道构成");
             row2.createCell(colNum++).setCellValue("东进口");
             row2.createCell(colNum++).setCellValue("西进口");
@@ -356,7 +293,7 @@ public class ExcelUtils {
             headerNum += 6;
         }
         if (lightHeader.toString().length()>0) {
-            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum,colNum+5));
+            sheet.addMergedRegion(new CellRangeAddress(1,1,colNum,colNum+5));
             row1.createCell(colNum).setCellValue("信号灯组");
             row2.createCell(colNum++).setCellValue("东进口");
             row2.createCell(colNum++).setCellValue("西进口");
@@ -367,7 +304,7 @@ public class ExcelUtils {
             headerNum += 6;
         }
         if (timingHeader.toString().length()>0) {
-            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum,colNum+12));
+            sheet.addMergedRegion(new CellRangeAddress(1,1,colNum,colNum+12));
             row1.createCell(colNum).setCellValue("配时信息");
             row2.createCell(colNum++).setCellValue("干线协调");
             row2.createCell(colNum++).setCellValue("放行方式");
@@ -385,21 +322,27 @@ public class ExcelUtils {
             headerNum += 13;
         }
         if (updatetimeFlag){
-            sheet.addMergedRegion(new CellRangeAddress(1,2,headerNum-1,headerNum-1));
-            row1.createCell(colNum++).setCellValue("更新日期");
+            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum++,colNum-1));
+            row1.createCell(colNum-1).setCellValue("更新日期");
         }
         if (noteFlag){
-            sheet.addMergedRegion(new CellRangeAddress(1,2,headerNum,headerNum));
-            row1.createCell(colNum++).setCellValue("备注");
+            sheet.addMergedRegion(new CellRangeAddress(1,2,colNum,colNum));
+            row1.createCell(colNum).setCellValue("备注");
         }
 
+    }
+
+    public static void setAlignCenter(Workbook workbook,HSSFCell cell){
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cell.setCellStyle(cellStyle);
     }
 
     public static void main(String[] args) throws IOException {
         ExcelModel data = new ExcelModel();
         List<ExcelDemo> list = new ArrayList<>();
         ExcelDemo demo = new ExcelDemo();
-        String str = "{\"crossname\":\"fassfsaf\",\"crossno\":\"fsasljfl\",\"crosstype\":null,\"managerunit\":null,\"manager\":null,\"degree\":null,\"isenabled\":null,\"networkenabled\":null,\"webtype\":null,\"electricannno\":null,\"crossnumber\":null,\"updatetime\":null,\"note\":null,\"annfactory\":null,\"anntype\":null,\"devicemaintain\":null,\"laneEentrance\":null,\"laneWentrance\":null,\"laneSentrance\":null,\"laneNentrance\":null,\"laneotherentrance\":null,\"turnrighttype\":null,\"lightEentrance\":null,\"lightWentrance\":null,\"lightSentrance\":null,\"lightNentrance\":null,\"lightotherentrance\":null,\"righttrafficlightcontrol\":null,\"trunklinecoordination\":null,\"releasetype\":null,\"specialcontrol\":null,\"selfadaptioncontrol\":null,\"yellowtwinkle\":null,\"flownum\":null,\"phasetimenum\":null,\"variance\":null,\"maxpersonwaitingtime\":null,\"maxcarwaitingtime\":null,\"maxperiod\":null,\"minperiod\":null,\"isadjustable\":null}";
+        String str = "{\"crossname\":\"fassfsaf\",\"crossno\":\"fsasljfl\",\"crosstype\":\"fassfsaf\",\"managerunit\":\"fassfsaf\",\"manager\":\"fassfsaf\",\"degree\":\"fassfsaf\",\"isenabled\":\"fassfsaf\",\"networkenabled\":\"fassfsaf\",\"webtype\":\"fassfsaf\",\"electricannno\":\"fassfsaf\",\"crossnumber\":\"fassfsaf\",\"updatetime\":\"fassfsaf\",\"note\":\"fassfsaf\",\"annfactory\":null,\"anntype\":null,\"devicemaintain\":null,\"laneEentrance\":\"fassfsaf\",\"laneWentrance\":\"fassfsaf\",\"laneSentrance\":\"fassfsaf\",\"laneNentrance\":\"fassfsaf\",\"laneotherentrance\":\"fassfsaf\",\"turnrighttype\":\"fassfsaf\",\"lightEentrance\":null,\"lightWentrance\":null,\"lightSentrance\":null,\"lightNentrance\":null,\"lightotherentrance\":null,\"righttrafficlightcontrol\":null,\"trunklinecoordination\":\"fassfsaf\",\"releasetype\":\"fassfsaf\",\"specialcontrol\":\"fassfsaf\",\"selfadaptioncontrol\":\"fassfsaf\",\"yellowtwinkle\":\"fassfsaf\",\"flownum\":\"fassfsaf\",\"phasetimenum\":\"fassfsaf\",\"variance\":\"fassfsaf\",\"maxpersonwaitingtime\":\"fassfsaf\",\"maxcarwaitingtime\":\"fassfsaf\",\"maxperiod\":\"fassfsaf\",\"minperiod\":\"fassfsaf\",\"isadjustable\":\"fassfsaf\"}";
         demo = new ObjectMapper().readValue(str,ExcelDemo.class);
         data.setDemoList(list);
         list.add(demo);
