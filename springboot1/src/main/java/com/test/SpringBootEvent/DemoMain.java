e6a4b2b3932362af6d1bf2cb584c7473ceacaece
JackyLee <1192964153@qq.com>
2019-05-15 11:33:25 +0800

    update

--- springboot1/src/main/java/com/test/SpringBootEvent/DemoMain.java
+++ springboot1/src/main/java/com/test/SpringBootEvent/DemoMain.java
@@ -1,10 +1,7 @@
 package com.test.SpringBootEvent;
 
+import com.test.SpringBootEvent.TaskExecutor.TaskExecutorService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
-
-import java.util.Iterator;
 
 /**
  * @Author : liyongjie
@@ -16,22 +13,22 @@ public class DemoMain {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
-//        DemoPublisher bean = context.getBean(DemoPublisher.class);
-//        bean.publish("Demo Event....................");
-//        AwareService bean1 = context.getBean(AwareService.class);
-//        bean1.outPutResult();
-//        TaskExecutorService bean = context.getBean(TaskExecutorService.class);
-//        for(int i =1;i<=10;i++){
-//            bean.asyncTask(i);
-//            bean.asyncTask2(i);
-//        }
-        MutablePropertySources s = context.getEnvironment().getPropertySources();
-        Iterator<PropertySource<?>> iterator = s.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next().getName());
+        DemoPublisher bean = context.getBean(DemoPublisher.class);
+        bean.publish("Demo Event....................");
+        AwareService bean1 = context.getBean(AwareService.class);
+        bean1.outPutResult();
+        TaskExecutorService executorService = context.getBean(TaskExecutorService.class);
+        for(int i =1;i<=10;i++){
+            executorService.asyncTask(i);
+            executorService.asyncTask2(i);
         }
-        System.out.println(s);
-        context.close();
+//        MutablePropertySources s = context.getEnvironment().getPropertySources();
+//        Iterator<PropertySource<?>> iterator = s.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.next().getName());
+//        }
+//        System.out.println(s);
+//        context.close();
     }
 
 }
