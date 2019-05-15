6ba7a86e84ec8a02640d5c06ee64a6dfc2fde052
JackyLee <1192964153@qq.com>
2019-05-15 13:49:29 +0800

    update

--- springboot1/src/main/java/com/test/SpringBootEvent/DemoMain.java
+++ springboot1/src/main/java/com/test/SpringBootEvent/DemoMain.java
@@ -11,17 +11,23 @@ public class DemoMain {
 
     public static void main(String[] args){
 
+        //
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
+        //事件监听
         DemoPublisher bean = context.getBean(DemoPublisher.class);
         bean.publish("Demo Event....................");
         AwareService bean1 = context.getBean(AwareService.class);
         bean1.outPutResult();
+
+        //异步调用
         TaskExecutorService executorService = context.getBean(TaskExecutorService.class);
         for(int i =1;i<=10;i++){
             executorService.asyncTask(i);
             executorService.asyncTask2(i);
         }
+
+
 //        MutablePropertySources s = context.getEnvironment().getPropertySources();
 //        Iterator<PropertySource<?>> iterator = s.iterator();
 //        while(iterator.hasNext()){
--- springboot1/src/main/java/com/test/SpringBootEvent/TaskExecutor/TaskExecutorService.java
+++ springboot1/src/main/java/com/test/SpringBootEvent/TaskExecutor/TaskExecutorService.java
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskExecutorService {
 
+    //标注这是一个异步方法，将会在独立的线程中执行
+    //不可以修饰static方法
+    // Thread.currentThread().getName() —> ThreadPoolTaskExecutor-1
     @Async
     public void asyncTask(Integer i){
-        System.out.println("执行异步任务：   " +i+"---->"+Thread.currentThread().getName());
+        System.out.println("执行异步任务：   " + i + "---->" + Thread.currentThread().getName());
     }
 
+    // Thread.currentThread().getName() —> main
     public void asyncTask2(Integer i){
-        System.out.println("执行异步任务+1：    " +(i+1)+"---->"+Thread.currentThread().getName());
+        System.out.println("执行异步任务+1：    " + i +" ----> "+ Thread.currentThread().getName());
     }
 }
